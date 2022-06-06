1e27ecdfae015392d010bf04de364b06c139249c
omero2582 <iiomero2582ii@gmail.com>
2022-06-06 09:46:56 -0400

    Clean up api & Book return obj, add sorting & css
    need to clean up how sorting works vs redering in terms of which
    one will be the 1 function called that will automatically call
    the other inside. Need to change this & perhaps sortMethod global var
    so I can use sortBy(method) by itself & have it render as part of
    the API.
    
    Add _ under private functions & more css. For css add space between
    header and the Library, space between header inputs, set
    header dropdown width, and change all colors prob. Also fix/choose
    the library grid, in terms of how we want the initial row to behave.
    Right now the grid is centered, so adding a new book while sorted by
    Newest feels unintuitive on the 1st row. But setting alignment to
    flex start puts it too close to border. Maybe use flex start + dynamic
    margin

--- script3.js
+++ script3.js
@@ -2,6 +2,8 @@
 
 let Library = (() =>{
     let allBooks = [];
+    let bookId = 0; //counting up
+    let sortMethod;
 
     //cache
     const modal = document.querySelector('.modal');
@@ -17,12 +19,17 @@ let Library = (() =>{
     const authorInput = document.querySelector('#book-author');
     const pagesInput = document.querySelector('#num-pages');
     const readStatusInput = document.querySelector('#read-status');
+    const sortInput = document.querySelector('#sort');
+    sortMethod = sortInput.value;
 
     //bind
     btnNewBook.addEventListener('click', openModal);
     window.addEventListener('click', checkTarget);
     btnClose.addEventListener('click', closeModal);
     bookForm.addEventListener('submit', submitForm);
+    sortInput.addEventListener('change', () =>{
+        sortMethod = sortInput.value;
+        renderAllBooks() });
 
     //modal
     function openModal(e){
@@ -42,7 +49,7 @@ let Library = (() =>{
     //DOM
     function submitForm(e){
         e.preventDefault();
-        let newBook = Book(titleInput.value, authorInput.value, pagesInput.value, readStatusInput.value);
+        let newBook = Book(bookId, titleInput.value, authorInput.value, pagesInput.value, readStatusInput.value);
        
         addBook(newBook);
         closeModal();
@@ -53,7 +60,7 @@ let Library = (() =>{
     function addBook (newBook){
         allBooks.push(newBook);
         renderAllBooks();
-         // console.log(newBook);
+        bookId++;
     }
     
     function deleteBook(index){
@@ -61,46 +68,88 @@ let Library = (() =>{
         allBooks.splice(index, 1);
         renderAllBooks();
     }
-    
+
     function renderBook(book){
         let clone = template.content.cloneNode(true);
-        clone.querySelector('.title-display').textContent = book.getTitle();
-        clone.querySelector('.author-display').textContent = book.getAuthor();
         let pageString = book.getNumPages()  > 1 ? 'pages' : 'page';
-        clone.querySelector('.pages-display').textContent = book.getNumPages() + ` ${pageString}`;
-        clone.querySelector('.read-display').value = book.getReadStatus();
+        let readDisplay = clone.querySelector('#read-display');
         let index =  allBooks.indexOf(book);
+        clone.querySelector('.title-display').textContent = book.getTitle();
+        clone.querySelector('.author-display').textContent = book.getAuthor();  
+        clone.querySelector('.pages-display').textContent = book.getNumPages() + ` ${pageString}`;
+        readDisplay.value = book.getReadStatus();
+        readDisplay.addEventListener('change', () => book.setReadStatus(readDisplay.value));
+        
         clone.querySelector('.book').setAttribute('data-key', index);
         clone.querySelector('.delete-btn').addEventListener('click', ()=> deleteBook(index));
         return clone;
     }
     
     function renderAllBooks(){
+        sortBy(sortMethod);
         bookDisplay.innerHTML="";
         allBooks.forEach( (book) =>{
             bookDisplay.appendChild(renderBook(book));
         });
-        console.table(allBooks);
+        logBooks();
     }
     
-    function sortBy(){
-    
+    function sortBy(method){
+        switch(method){
+            case 'Newest Added':
+                allBooks.sort( (a, b) => b.getBookId() - a.getBookId());
+                break;
+            case 'Oldest Added':
+                allBooks.sort( (a, b) => a.getBookId() - b.getBookId());
+                break;
+            case 'Title A-Z':
+                allBooks.sort( (a,b) => a.getTitle().localeCompare(b.getTitle()));
+                break;
+            case 'Title Z-A':
+                allBooks.sort( (a,b) => b.getTitle().localeCompare(a.getTitle()));
+                break;
+            case 'Author A-Z':
+                allBooks.sort( (a,b) => a.getAuthor().localeCompare(b.getAuthor()));
+                break;
+            case 'Author Z-A':
+                allBooks.sort( (a,b) => b.getAuthor().localeCompare(a.getAuthor()));
+                break;
+            case 'Pages Descending':
+                allBooks.sort( (a,b) => b.getNumPages() - a.getNumPages());
+                break;
+            case 'Pages Ascending':
+                allBooks.sort( (a,b) =>  a.getNumPages() - b.getNumPages());
+                break;
+            default:
+                console.log(`sorting method '${sortMethod}' not supported`);
+        }
+        console.log(`sorted by ${sortMethod}`);
     }
 
-    return {addBook, deleteBook}
+    function logBooks(){
+        console.table(allBooks.map( one => one.toObj()));
+    }
+
+    return {addBook, deleteBook, logBooks, allBooks, bookId, sortMethod}
 
 })();
 
 
-function Book(title, author, numPages, readStatus){
+function Book(bookId, title, author, numPages, readStatus){
+    const getBookId = () => bookId;
     const getTitle = () => title;
     const getAuthor = () => author;
     const getNumPages = () => numPages;
     const getReadStatus = () => readStatus;
+    const setReadStatus = (status) => {
+        readStatus = status; console.log(`read Status of ${title} set to ${status}`);
+        Library.logBooks();
+    };
+    const toObj = () => {
+        return {bookId, title, author, numPages, readStatus}
+    }
 
-    // return {getTitle, getAuthor, getNumPages, getReadStatus}
-    return {title, author, numPages, readStatus,
-            getTitle, getAuthor, getNumPages, getReadStatus}    //testing
+    return {getBookId, getTitle, getAuthor, getNumPages, getReadStatus, setReadStatus, toObj}   
 }
 
 
